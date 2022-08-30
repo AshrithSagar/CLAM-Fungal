1109c60167c3ba6c009770c71009f8bb979de515
Ashrith Sagar <ashrith9sagar@gmail.com>
2022-08-30 07:41:30 +0530

    store_dir in hdf5_creator.py

--- hdf5_creator.py
+++ hdf5_creator.py
@@ -8,18 +8,21 @@ import h5py
 parser = argparse.ArgumentParser(description='Initialize .h5 files')
 parser.add_argument('--source', type = str,
                     help='Path to folder containing the image folders of patches')
+parser.add_argument('--dest', type = str,
+                    help='Path to folder to store the .h5 files')
 
 
 if __name__ == '__main__':
     args = parser.parse_args()
 
     patch_dir = args.source
+    store_dir = args.dest
 
     for folder in os.listdir(patch_dir):
         patch_folder = os.path.join(patch_dir, folder)
         for patch_file in os.listdir(patch_folder):
             name, ext = os.path.splitext(patch_file)
-            file_path = os.path.join(patch_dir, name)+'.h5'
+            file_path = os.path.join(store_dir, name)+'.h5'
             file = h5py.File(file_path, "w")
 
             file.close()
