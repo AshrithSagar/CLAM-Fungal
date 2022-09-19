9b2f3a332dd4233277809ef0c62e4ea4056e34a6
Ashrith Sagar <ashrith9sagar@gmail.com>
2022-09-19 18:34:27 +0530

    Indent using spaces

--- imgs_creator.py
+++ imgs_creator.py
@@ -9,25 +9,25 @@ from PIL import Image
 
 parser = argparse.ArgumentParser(description='Image patches to numpy')
 parser.add_argument('--source', type = str,
-					help='Path to folder containing the image folders of patches')
+                    help='Path to folder containing the image folders of patches')
 
 
 if __name__ == '__main__':
-	args = parser.parse_args()
+    args = parser.parse_args()
 
-	patch_dir = args.source
+    patch_dir = args.source
 
-	for folder in os.listdir(patch_dir):
-	    patch_folder = os.path.join(patch_dir, folder)
-	    for patch_file in os.listdir(patch_folder):
-			img_path = os.path.join(patch_folder, patch_file)
-			img = Image.open(img_path)
+    for folder in os.listdir(patch_dir):
+        patch_folder = os.path.join(patch_dir, folder)
+        for patch_file in os.listdir(patch_folder):
+            img_path = os.path.join(patch_folder, patch_file)
+            img = Image.open(img_path)
 
-			img_arr = np.asarray(img)
-			img_PIL = Image.fromarray(img_arr)
+            img_arr = np.asarray(img)
+            img_PIL = Image.fromarray(img_arr)
 
-			print(img_PIL)
+            print(img_PIL)
 
-			print("="*50)
+            print("="*50)
 
-			img.close()
+            img.close()
