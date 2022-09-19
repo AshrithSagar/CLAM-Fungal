19709b20577ee713de17beb7c2a77b218e4994d1
Ashrith Sagar <ashrith9sagar@gmail.com>
2022-09-19 18:50:58 +0530

    removed print

--- imgs_creator.py
+++ imgs_creator.py
@@ -26,8 +26,4 @@ if __name__ == '__main__':
             img_arr = np.asarray(img)
             img_PIL = Image.fromarray(img_arr)
 
-            print(dir(img_PIL))
-
-            print("="*50)
-
             img.close()
