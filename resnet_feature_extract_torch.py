068bacd7083931c4df2ea7af933df81c571caa80
Ashrith Sagar <ashrith9sagar@gmail.com>
2022-10-06 07:57:02 +0530

    resnet_custom in resnet_feature_extract

--- resnet_feature_extract_torch.py
+++ resnet_feature_extract_torch.py
@@ -11,27 +11,39 @@ from models.resnet_custom import resnet50_baseline
 
 device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
 
+
+def extract(path):
+    with torch.no_grad():
+        features = model()
+        print(features)
+
+
 parser = argparse.ArgumentParser(description='Extract features using RESNET')
 parser.add_argument('--source', type = str,
     help='Path to folder containing the image folders of patches')
+parser.add_argument('--output', type = str,
+    help='Path to folder for storing the feature vectors')
 args = parser.parse_args()
 
 
 if __name__ == '__main__':
     patch_dir = args.source
+    feat_dir = args.output
 
-    model_path = os.path.join(os.getcwd(), 'image_sets/resnet50-19c8e357.pth')
-    model = torch.load(model_path)
-    # model = model.to(device)
+    model = resnet50_baseline(pretrained=True)
+    model = model.to(device)
+
     if torch.cuda.device_count() > 1:
         model = nn.DataParallel(model)
+
     model.eval()
 
     for folder in os.listdir(patch_dir):
         patch_folder = os.path.join(patch_dir, folder)
         for patch_file in os.listdir(patch_folder):
             img_path = os.path.join(patch_folder, patch_file)
-            with torch.no_grad():
-                features = model(model)
+
+            features = extract(img_path)
+
             break
         break
