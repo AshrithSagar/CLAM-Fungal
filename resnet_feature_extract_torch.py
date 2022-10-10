f556e8d0a6787b0b8e7b2c455d3266b591f615bc
Ashrith Sagar <ashrith9sagar@gmail.com>
2022-10-10 21:09:26 +0530

    batch = batch.reshape([1, 3, 256, 256])

--- resnet_feature_extract_torch.py
+++ resnet_feature_extract_torch.py
@@ -46,7 +46,7 @@ def extract(img_path):
             print(batch.shape)
             batch = torch.unsqueeze(batch, 0)
             print(batch.shape)
-            batch = batch.reshape([0, 3, 2, 1])
+            batch = batch.reshape([1, 3, 256, 256])
             print(batch.shape)
             batch = batch.to(device, non_blocking=True)
             features = model(batch)
