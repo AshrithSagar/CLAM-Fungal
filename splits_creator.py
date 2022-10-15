27196dfbb900693627e11d36aedecc4a0cbd6347
Ashrith Sagar <ashrith9sagar@gmail.com>
2022-10-15 19:44:45 +0530

    fungal_vs_nonfungal.csv added; renamed in splits_creator

--- splits_creator.py
+++ splits_creator.py
@@ -11,7 +11,6 @@ parser.add_argument('--seed', type=int, default=1,
                     help='random seed (default: 1)')
 parser.add_argument('--k', type=int, default=10,
                     help='number of splits (default: 10)')
-parser.add_argument('--task', type=str, choices=['task_1_tumor_vs_normal', 'task_2_tumor_subtyping'])
 parser.add_argument('--val_frac', type=float, default= 0.1,
                     help='fraction of labels for validation (default: 0.1)')
 parser.add_argument('--test_frac', type=float, default= 0.1,
@@ -19,29 +18,15 @@ parser.add_argument('--test_frac', type=float, default= 0.1,
 
 args = parser.parse_args()
 
-if args.task == 'task_1_tumor_vs_normal':
-    args.n_classes=2
-    dataset = Generic_WSI_Classification_Dataset(csv_path = 'dataset_csv/tumor_vs_normal_dummy_clean.csv',
-                            shuffle = False,
-                            seed = args.seed,
-                            print_info = True,
-                            label_dict = {'normal_tissue':0, 'tumor_tissue':1},
-                            patient_strat=True,
-                            ignore=[])
-
-elif args.task == 'task_2_tumor_subtyping':
-    args.n_classes=3
-    dataset = Generic_WSI_Classification_Dataset(csv_path = 'dataset_csv/tumor_subtyping_dummy_clean.csv',
-                            shuffle = False,
-                            seed = args.seed,
-                            print_info = True,
-                            label_dict = {'subtype_1':0, 'subtype_2':1, 'subtype_3':2},
-                            patient_strat= True,
-                            patient_voting='maj',
-                            ignore=[])
-
-else:
-    raise NotImplementedError
+# task_1_fungal_vs_nonfungal
+args.n_classes=2
+dataset = Generic_WSI_Classification_Dataset(csv_path = 'dataset_csv/fungal_vs_nonfungal.csv',
+                        shuffle = False,
+                        seed = args.seed,
+                        print_info = True,
+                        label_dict = {'normal_tissue':0, 'tumor_tissue':1},
+                        patient_strat=True,
+                        ignore=[])
 
 num_slides_cls = np.array([len(cls_ids) for cls_ids in dataset.patient_cls_ids])
 val_num = np.round(num_slides_cls * args.val_frac).astype(int)
@@ -64,6 +49,3 @@ if __name__ == '__main__':
             save_splits(splits, ['train', 'val', 'test'], os.path.join(split_dir, 'splits_{}.csv'.format(i)))
             save_splits(splits, ['train', 'val', 'test'], os.path.join(split_dir, 'splits_{}_bool.csv'.format(i)), boolean_style=True)
             descriptor_df.to_csv(os.path.join(split_dir, 'splits_{}_descriptor.csv'.format(i)))
-
-
-
