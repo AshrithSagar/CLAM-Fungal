c41eb4e2b1bcc1015639f2fd83a8aa0c59c66174
Ashrith Sagar <ashrith9sagar@gmail.com>
2022-12-09 18:08:31 +0530

    dataset_csv_creator with yaml

--- dataset_csv_creator.py
+++ dataset_csv_creator.py
@@ -2,13 +2,34 @@
 Create the dataset .csv file
 Warning: The file is overwritten, so please save before proceeding.
 """
-
 import os
+import yaml
+import argparse
 
-filename = "dataset_csv/fungal_vs_nonfungal.csv"
-patch_dir = "image_sets/patches/"
-feat_dir = "image_sets/patches/fungal_vs_nonfungal_resnet_features/"
 
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Dataset CSV creator')
+    parser.add_argument('-c', '--config', type = str,
+                        help='Path to the config file')
+
+    parser.add_argument('--filename', type = str,
+                        help='dataset_csv filename')
+    parser.add_argument('--patch_dir', type = str,
+                        help='Path to folder containing the image folders of patches')
+    parser.add_argument('--feat_dir', type = str,
+                        help='Path to folder for storing the feature vectors')
+
+    args = parser.parse_args()
+    if args.config:
+        config = yaml.safe_load(open(args.config, 'r'))
+        args = config['dataset_csv_creator']
+
+    filename = args.filename
+    patch_dir = args.patch_dir
+    feat_dir = args.feat_dir
+
+
+# ----------------------------------------------------------------
 with open(filename, 'w') as file:
     file.write('case_id,slide_id,label' + '\n')
 
