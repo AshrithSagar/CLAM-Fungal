f8cf738f6a6eb771f20685a8696054efc90e8be9
Ashrith Sagar <ashrith9sagar@gmail.com>
2022-12-17 15:46:25 +0530

    dataset create csv annotated_dir

--- dataset_csv_creator.py
+++ dataset_csv_creator.py
@@ -18,6 +18,8 @@ if __name__ == '__main__':
                         help='Path to folder containing the image folders of patches')
     parser.add_argument('--feat_dir', type = str,
                         help='Path to folder for storing the feature vectors')
+    parser.add_argument('--annotated_dir', type = str,
+                        help='Path to folder containing the image folders of annotated patches')
 
     args = parser.parse_args()
     if args.config:
@@ -27,22 +29,30 @@ if __name__ == '__main__':
     filename = args['filename']
     patch_dir = args['patch_dir']
     feat_dir = args['feat_dir']
+    annotated_dir = args['annotated_dir']
 
 
 # ----------------------------------------------------------------
 with open(filename, 'w') as file:
-    file.write('case_id,slide_id,label' + '\n')
+    file.write('case_id,slide_id,label,annotated' + '\n')
 
     patch_folders = [os.path.join(patch_dir, folder) for folder in sorted(os.listdir(patch_dir))]
+    annotated_folders = os.listdir(annotated_dir)
 
     for i, name in enumerate(patch_folders):
         name = name.split("/")[-1]
-        if name != feat_dir:         
+        if name != feat_dir:
             if name[0] == "F":
                 f_nf = "fungal"
             elif name[0] == "N":
                 f_nf = "nonfungal"
             else:
                 f_nf = "unclassified"
-            line = 'case_' + str(i) + ',' + name + ',' + f_nf
+
+            if name not in annotated_folders:
+                annotated = False
+            else:
+                annotated = True
+
+            line = 'case_' + str(i) + ',' + name + ',' + f_nf + ',' + annotated
             file.write('{}\n'.format(line))
