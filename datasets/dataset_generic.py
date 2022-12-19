113e7d939dd123339494474295ac5ce793febc84
Ashrith Sagar <ashrith9sagar@gmail.com>
2022-12-19 15:54:40 +0530

    indent

--- datasets/dataset_generic.py
+++ datasets/dataset_generic.py
@@ -21,10 +21,10 @@ def save_splits(split_datasets, column_keys, annot_frac, filename, boolean_style
 
 	# Add annot column
 	train_set = splits[0]
-    train_set_list = []
-    for i in range(n_classes):
-        for ids in train_set.slide_cls_ids[i]:
-            train_set_list.append(str(train_set.slide_data['slide_id'][ids]))
+	train_set_list = []
+	for i in range(n_classes):
+		for ids in train_set.slide_cls_ids[i]:
+			train_set_list.append(str(train_set.slide_data['slide_id'][ids]))
 	annot_num = np.round(len(train_set_list) * annot_frac).astype(int)
 	annot_set = random.sample(train_set_list, annot_num)
 	true_annot_set = [True if (x in annot_set) else False for x in annot_set]
