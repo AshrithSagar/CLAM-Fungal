31d05302b2bb4cd6bc623ce07f8907297ee0678c
Ashrith Sagar <ashrith9sagar@gmail.com>
2023-01-14 16:22:51 +0530

    idx instead of slide_id

--- modules/dataset_generic.py
+++ modules/dataset_generic.py
@@ -421,7 +421,7 @@ class Generic_MIL_Dataset(Generic_WSI_Classification_Dataset):
             if self.data_dir:
                 full_path = os.path.join(data_dir, '{}.pt'.format(slide_id))
                 features = torch.load(full_path)
-                return features, slide_id, label, bool_annot, patch_annot
+                return features, idx, label, bool_annot, patch_annot
                 # return features, label
 
             else:
