c3a65f290151c251d5a45a66396a1d4182c02632
Ashrith Sagar <ashrith9sagar@gmail.com>
2023-01-18 15:02:00 +0530

    collate_MIL_mod bool_annot

--- modules/dataset_generic.py
+++ modules/dataset_generic.py
@@ -421,10 +421,10 @@ class Generic_MIL_Dataset(Generic_WSI_Classification_Dataset):
             if self.data_dir:
                 full_path = os.path.join(data_dir, '{}.pt'.format(slide_id))
                 features = torch.load(full_path)
-                # return features, label, idx, bool_annot, patch_annot
+                print("__get_item__", slide_id, idx, label, bool_annot, patch_annot)
+                return features, label, idx, bool_annot, patch_annot
                 # return features, label
-                print("__get_item__", slide_id, idx, label)
-                return features, label, idx
+                # return features, label, idx
 
             else:
                 # if bool_annot:
