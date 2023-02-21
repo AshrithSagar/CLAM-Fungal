c1337fe4771ff96b778a3030f29336b6acc6d52c
Ashrith Sagar <ashrith9sagar@gmail.com>
2023-02-21 15:05:46 +0530

    zip_results split

--- zip_results.sh
+++ zip_results.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 
+# Usage
+# ./zip_results.sh exp_00_s1 splits_0 filename_for_zip
+
 exp=$1
+split=$2
+file=$3
 
 result_dir="experiments/results/"
 outputdir="experiments/heatmaps/"
 
-outputfile=${file}.zip
-contents=${result_dir}
+outputfile="${outputdir}${file}.zip"
+contents="${result_dir}${exp}/${split}/heatmaps/*"
 
 zip ${outputfile} ${contents}
